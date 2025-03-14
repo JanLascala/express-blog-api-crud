dcfe55166495da84b9f62e45fc77511374ad503f
JanLascala <lascala.jan@gmail.com>
2025-03-14 15:50:25 +0100

    Add function exported funtion to routers linked

--- controllers/posts_controllers.js
+++ controllers/posts_controllers.js
@@ -1,9 +1,63 @@
 // main require require
 const posts = require('../data/posts.js')
 
-function index()
-function show()
-function store()
-function update()
-function patch()
-function destroy()
+//index
+function index(req, res) {
+    console.log(req);
+};
+//show
+function show(req, res) {
+    function show(req, res) {
+        let post = posts.find(post => post.slug === req.query.slug);
+
+        if (!post) {
+            return res.status(404).json({ message: "Post not found" });
+        }
+
+        res.json(post);
+    }
+};
+//store
+function store(req, res) {
+    res.send('post new posts')
+};
+//update
+function update(req, res) {
+    res.send('update posts')
+};
+//modify or patch
+function patch(req, res) {
+    res.send('patch this post')
+};
+//destroy!! destroy!!
+function destroy(req, res) {
+
+    //get mit slug
+    const postsSlug = req.params.slug
+    // find the post mit id
+    const postsIndex = posts.find(posts => posts.slug === postsSlug);
+    console.log(postsIndex)
+
+    //if not existing formula
+    if (!postsIndex) {
+        return res.status(404).json({
+            error: 'post not found',
+            message: 'post not found',
+        })
+    }
+    res.json(postsSlug)
+
+    //removal
+    posts.splice(posts.indexOf(postsSlug), 1)
+    console.log(posts)
+
+    res.sendStatus(204)
+}
+module.exports = {
+    index,
+    show,
+    store,
+    update,
+    patch,
+    destroy,
+}
--- routers/posts_routers.js
+++ routers/posts_routers.js
@@ -3,22 +3,24 @@ const express = require('express');
 const router = express.Router();
 //export posts jinc
 const posts = require('../data/posts.js')
+//export controllers
+const postsControllers = require('../controllers/posts_controllers.js')
 
 
 //index
-router.get('/',);
+router.get('/', postsControllers.index);
 
 //show
-router.get('/slug',);
+router.get('/slug', postsControllers.show);
 
 //store
-router.post('/',);
+router.post('/', postsControllers.store);
 
 //update
-router.put('/slug',);
+router.put('/slug', postsControllers.update);
 
 // patch/modify
-router.patch('/slug',);
+router.patch('/slug', postsControllers.patch);
 
 //destroy
-router.delete('/slug',);
+router.delete('/slug', postsControllers.destroy);
