30756f4e098544fb63dc89dda0143d0b16ab8468
JanLascala <lascala.jan@gmail.com>
2025-03-14 15:57:49 +0100

    Set routes on server

--- server.js
+++ server.js
@@ -4,9 +4,15 @@ const app = express();
 const port = 3000;
 //posts main data
 const posts = require('./data/posts.js')
+// export routers
+const postsRouters = require('./routers/posts_routers.js')
+
 //add static image? here later??
 
 //server listen
 app.listen(port, () => {
     console.log(`server listening http://localhost:${port}`);
 })
+
+//set path for routes !!!!!!! remember to do it at the set up !!!!!!!!
+app.use('/api/v1/posts', postsRouters);
